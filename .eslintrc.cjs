4c05fcf70ff03cfff96edade3c650722b9d8c83e
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-07-26 02:23:36 +0300

    feat!: setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:unicorn/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort', 'import', 'promise'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
