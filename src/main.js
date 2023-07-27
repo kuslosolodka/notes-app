56fea4908e1027fc9f4a4a57ebe4c3f169293e52
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-07-27 23:28:11 +0300

    feat: + app component

--- src/main.js
+++ src/main.js
@@ -1,24 +1,6 @@
-// eslint-disable-next-line import/no-unresolved
-import javascriptLogo from '../javascript.svg';
-import viteLogo from '../public/vite.svg';
-import setupCounter from './counter';
+import { Notes } from './pages/notes';
+// eslint-disable-next-line no-unused-vars
+import styles from './styles/global-styles.module.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector('#counter'));
+const app = new Notes();
+app.render();
