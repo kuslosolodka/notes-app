ba7d1a01104ea2f0bda9c03c64d9e375fa0d158b
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-07-28 12:55:58 +0300

    fix: using of styles in app component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { Notes } from './pages/notes';
-// eslint-disable-next-line no-unused-vars
 import styles from './styles/global-styles.module.css';
 
 const app = new Notes();
-app.render();
+app.render(styles);
